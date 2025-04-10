5778f91533fc4c74a00270c42b48fca44d5ac072
Tyler Lott <=>
2025-04-09 21:35:18 -0500

    init

---
+++ LitLoot/LitLoot/LitLootApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitLootApp.swift
+//  LitLoot
+//
+//  Created by Tyler Lott on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LitLootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
